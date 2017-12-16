2bf213ab8bf934395f6ef52fccfec3652f9fcb13
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 19:01:35 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |s|
+      s.string :title
+      s.integer :artist_id
+      s.integer :genre_id
+    end
+  end
 end
