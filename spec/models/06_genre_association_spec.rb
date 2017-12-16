4173b033e463f5ec5bf1bbb5687b0e117e6a6ede
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 19:33:04 +0000

    automatically backed up by learn

--- spec/models/06_genre_association_spec.rb
+++ spec/models/06_genre_association_spec.rb
@@ -6,16 +6,16 @@ describe 'Genre Associations' do
   end
 
   it 'has many songs' do
-    @genre.songs << Song.create(name: "Something By That Person Who Sings Stuff")
+    @genre.songs << Song.create(title: "Something By That Person Who Sings Stuff")
     @genre.save
 
-    found_song = Song.find_by(name: "Something By That Person Who Sings Stuff")
+    found_song = Song.find_by(title: "Something By That Person Who Sings Stuff")
     expect(found_song.genre).to eq(@genre)
   end
 
   it 'is also associated with an artist' do
     artist = Artist.create(name: "Fun Person Who Sings")
-    song = Song.create(name: "Sweet Tunez", genre: @genre)
+    song = Song.create(title: "Sweet Tunez", genre: @genre)
     artist.songs << song
     artist.save
 
